4d9ef7ee2842a7f845cc7f976b324771e44bbb77
sasin_99@hotmail.com <sasin_99@hotmail.com@cdda2122-1f54-0410-a529-49cf073505f5>
2010-07-28 11:03:54 +0000

    시계에 넣을까 고민중인 부분...

--- trunk/AlarmGitai/Classes/CharView.h
+++ trunk/AlarmGitai/Classes/CharView.h
@@ -3,6 +3,9 @@
 @interface CharView : UIView {
 	UIImage* imgBase;
 	UIImage* imgFace;
+
+	UIImage* baseImg;
+	UIImage* maskImg;
 }
 
 - (void)setChar:(NSString*)name idx:(int)idx isNight:(bool)isNight;
