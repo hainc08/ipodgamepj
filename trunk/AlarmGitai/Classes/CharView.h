373e7d9e503201afff3a69aa33af4019c6f07f2b
sasin_99@hotmail.com <sasin_99@hotmail.com@cdda2122-1f54-0410-a529-49cf073505f5>
2010-08-11 08:16:48 +0000

    배경보여주기 - 가로세로 처리 해야함

--- trunk/AlarmGitai/Classes/CharView.h
+++ trunk/AlarmGitai/Classes/CharView.h
@@ -1,13 +1,13 @@
 #import <UIKit/UIKit.h>
 
 @interface CharView : UIView {
+	UIImage* imgBack;
+
 	UIImage* imgBase;
 	UIImage* imgFace;
-
-	UIImage* baseImg;
-	UIImage* maskImg;
 }
 
 - (void)setChar:(NSString*)name idx:(int)idx isNight:(bool)isNight;
+- (void)setBackGround:(int)idx isNight:(bool)isNight;
 
 @end
