d1a8f7882762f88d7027c4e04cb9e04bd0cd47b6
embmaster <embmaster@nate.com>
2010-11-03 13:19:45 +0000

    * Shake File Add {{{ A    AlarmGitai/Classes/AlarmShakeController.h A    AlarmGitai/Classes/AlarmShakeController.m M    AlarmGitai/AlarmGitai.xcodeproj/project.pbxproj }}}

---
+++ AlarmGitai/Classes/AlarmShakeController.h
@@ -0,0 +1,16 @@
+//
+//  AlarmShakeController.h
+//  AlarmGitai
+//
+//  Created by embmaster on 10. 11. 02.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AlarmShakeController : UIViewController {
+	UIButton *EndButton;
+}
+
+@end
