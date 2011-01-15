e4fcdd1cd83d27240c8b9d8ebd50d91b65467bb4
embmaster@nate.com <embmaster@nate.com@cdda2122-1f54-0410-a529-49cf073505f5>
2011-01-15 10:14:13 +0000

    #Sound Manager {{{ AM   BaseClasses/SoundManager.h AM   BaseClasses/SoundManager.m }}}

---
+++ trunk/AlarmGitaiFinal/BaseClasses/SoundManager.h
@@ -0,0 +1,26 @@
+#import <AVFoundation/AVFoundation.h>
+
+@class AlarmDate;
+
+@interface SoundManager : NSObject <AVAudioPlayerDelegate> {
+	AVAudioPlayer* curAlarmPlayer;
+	NSTimer *vibrationTimer;
+	AlarmDate *		Alarm; 
+}
+
++ (SoundManager*)getInstance;
++ (void)initManager;
+- (void)closeManager;
+- (void)reset;
+
+-(void) setAlarmVolume : (float) level; 
+
+-(void)playAlarm:(AlarmDate *)_inData;
+-(void)stopAlarm;
+- (void)resumeTimer;
+- (void)vibration;
+
+-(void)playSound:(NSString *) _inSoundName ;
+-(void)stopSound;
+
+@end
