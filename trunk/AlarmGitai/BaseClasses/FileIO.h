dad56015f16f3a903af166f6c66447a22c15ab4d
sasin_99@hotmail.com <sasin_99@hotmail.com@cdda2122-1f54-0410-a529-49cf073505f5>
2011-02-18 14:34:52 +0000

    지우고...

--- trunk/AlarmGitai/BaseClasses/FileIO.h
+++
@@ -1,6 +0,0 @@
-int readInt(NSFileHandle* readFile);
-void writeInt(NSFileHandle* writeFile, int value);
-float readFloat(NSFileHandle* readFile);
-void writeFloat(NSFileHandle* writeFile, float value);
-void writeString(NSFileHandle* writeFile, NSString* value);
-NSString* readString(NSFileHandle* readFile);
--- trunk/AlarmGitai/Classes/AlarmGitaiViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface AlarmGitaiViewController : UIViewController {
-	IBOutlet UIImageView* touch;
-}
-
-@end
-
