2498d3be82074c6c624be651c33e7fd9039de2e7
sasin_99@hotmail.com <sasin_99@hotmail.com@cdda2122-1f54-0410-a529-49cf073505f5>
2011-02-18 16:21:09 +0000

    롯데리아

---
+++ trunk/lotteria/Classes/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  lotteria
+//
+//  Created by Sasin on 11. 2. 18..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate> {
+}
+
+- (IBAction)showInfo:(id)sender;
+
+@end
