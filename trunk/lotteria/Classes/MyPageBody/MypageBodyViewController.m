e3827635d21cc7b7d7e09aa94004bbbe8f893e46
embmaster@nate.com <embmaster@nate.com@cdda2122-1f54-0410-a529-49cf073505f5>
2011-02-21 15:47:49 +0000

    M    lotteria/lotteria.xcodeproj/project.pbxproj A    lotteria/baseClass/HttpRequest.m A    lotteria/baseClass/HttpRequest.h M    lotteria/ISPPay_TEST/Classes/ISPPayController.m A    lotteria/Classes/MyPageBody/LoginViewController.m A    lotteria/Classes/MyPageBody/LoginView.xib M    lotteria/Classes/MyPageBody/MypageBodyViewController.h M    lotteria/Classes/MyPageBody/MypageBodyViewController.m M    lotteria/Classes/MyPageBody/MypageBodyView.xib A    lotteria/Classes/MyPageBody/LoginViewController.h

--- trunk/lotteria/Classes/MyPageBody/MypageBodyViewController.m
+++ trunk/lotteria/Classes/MyPageBody/MypageBodyViewController.m
@@ -2,8 +2,12 @@
 
 @implementation MypageBodyViewController
 
+@synthesize Login;
+
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
+	
 }
 
 - (void)viewDidUnload {
@@ -15,5 +19,27 @@
     [super dealloc];
 }
 
+
+- (IBAction)OrderListButton
+{
+	//Login이 된 플레그가 0 이면 login페이지로 1이면 List 페이지 이동 
+	//if () 
+	{
+		LoginViewController *Login_tmp = [[LoginViewController alloc] initWithNibName:@"LoginView" bundle:nil];
+		self.Login = Login_tmp;
+		self.Login.delegate = self;
+		[self.view insertSubview:Login_tmp.view atIndex:9];
+		
+		[Login_tmp release];
+	}
+}
+
+
+#pragma mark -
+#pragma mark LoginViewDelegate
+
+-(void)returnLoginValue:(NSString *)LoginInfo
+{
+}
 
 @end
