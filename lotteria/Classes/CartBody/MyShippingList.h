cb9ba61f9b3ba82ec98b40a610658a0ca44fd544
embmaster <embmaster@nate.com>
2011-02-24 01:20:06 +0000

    # Shipping List A    CartBody/MyShippingList.xib A    CartBody/MyShippingList.h A    CartBody/MyShippingList.m

---
+++ lotteria/Classes/CartBody/MyShippingList.h
@@ -0,0 +1,24 @@
+//
+//  MyShippingList.h
+//  lotteria
+//
+//  Created by embmaster on 11. 2. 24..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIViewControllerTemplate.h"
+
+@class Order;
+
+@interface MyShippingList : UIViewControllerTemplate<UITableViewDataSource, UITableViewDelegate > {
+	IBOutlet UITableView *CustomerTable;
+    NSMutableArray *CustomerArr;
+	
+	Order	*InfoOrder;
+}
+@property (nonatomic, retain)     NSMutableArray *CustomerArr;
+@property (nonatomic, retain) IBOutlet UITableView *CustomerTable;
+@property (nonatomic, retain) Order *InfoOrder;
+- (void)ShowOKAlert:(NSString *)title msg:(NSString *)message;
+@end
