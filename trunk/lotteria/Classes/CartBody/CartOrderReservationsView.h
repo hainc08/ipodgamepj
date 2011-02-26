ab8a107e1b8281972c6c54ba95cfa7c75b3f37d5
embmaster@nate.com <embmaster@nate.com@cdda2122-1f54-0410-a529-49cf073505f5>
2011-02-26 13:28:34 +0000

    #CartOrderReservationView A    CartBody/CartOrderReservationsView.m A    CartBody/CartOrderReservationsView.xib A    CartBody/CartOrderReservationsView.h

---
+++ trunk/lotteria/Classes/CartBody/CartOrderReservationsView.h
@@ -0,0 +1,16 @@
+//
+//  CartOrderReservationsView.h
+//  lotteria
+//
+//  Created by embmaster on 11. 2. 26..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CartOrderReservationsView : UIViewController {
+
+}
+
+@end
