b0dc85130291e9b6ca3d26af277d272101902d28
embmaster <embmaster@nate.com>
2012-04-04 22:26:58 +0000

    으흠~;;;

--- Halloween/Object/Ghost.h
+++ Halloween/Object/Ghost.h
@@ -1,10 +1,20 @@
 #import "Object.h"
 
+enum GHOST_TYPE {
+    GHOST_NONE = 0,
+    GHOST_CANDY,
+    GHOST_DIE,
+    GHOST_SUCESS,
+    GHOST_END
+};
+
 @interface Ghost : Object {
+
 	UIImage* img[4];
 	
 	IBOutlet UIImageView* imgView;
 	int imgIdx;
+    int ghost_type;
 }
 
 - (id)initWithType:(int)type;
